2b684fef9d820ef01cd191d1c82bd773ce2c49a4
Ross Morgan <ross.morgan933@gmail.com>
2024-10-03 12:54:25 -0500

    post calls for books and authors working

---
+++ DTOs/CreateAuthorDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SimplyBooksAPI.DTOs
+{
+    public class CreateAuthorDTO
+    {
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Image { get; set; }
+        public bool Favorite { get; set; }
+        public string? Uid { get; set; }
+    }
+}
